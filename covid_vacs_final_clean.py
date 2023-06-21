bc1c9c137fadc39af7173a35c1f886650355d854
Dee-Kuromori <103159600+Dee-Kuromori@users.noreply.github.com>
2023-06-21 15:09:21 -0400

    Add files via upload

---
+++ covid_vacs_final_clean.py
@@ -0,0 +1,24 @@
+'''
+Drop uneeded rows from partially formatted covid-
+vaccination data.
+'''
+
+import csv
+
+with open(r'covid_vacs_not_clean.csv','r') as csv_file,open('covid_vacs_final.csv', 'w',newline='') as covid:
+
+
+ csv_reader = csv.reader(csv_file, delimiter = ',')
+ csv_writer = csv.writer(covid, delimiter=',')
+
+
+ #get headers
+
+ headers = next(csv_reader)
+ 
+ csv_writer.writerow(headers)
+
+ for row in csv_reader:
+    if row[3] < '2021-02-01' and row[3] > '2020-02-20':
+        csv_writer.writerow(row)
+        
